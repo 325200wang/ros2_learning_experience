f69b77d61d60199484365d9ffeb84dd9fefca6b7
325200wang <a18967707065@icloud.com>
2025-11-13 22:26:22 +0800

    11.13 study

---
+++ chapt1/CMakeFiles/learn_camke.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_camke.dir/world.cpp.o"
+  "CMakeFiles/learn_camke.dir/world.cpp.o.d"
+  "learn_camke"
+  "learn_camke.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn_camke.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapt1/CMakeFiles/learn_camke.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for learn_camke.
+# This may be replaced when dependencies are built.
