522bd3c89ef2c6cc4c00ca2961aa74dabb72fd9c
325200wang <a18967707065@icloud.com>
2025-11-17 20:46:23 +0800

    11.17

--- chapter2/chapt2_ws/install/demo_python_pkg/lib/python3.10/site-packages/demo_python_pkg/person_node.py
+++ chapter2/chapt2_ws/install/demo_python_pkg/lib/python3.10/site-packages/demo_python_pkg/person_node.py
@@ -7,6 +7,6 @@ class PersonNode:#类内部方法第一个参数都是self,代表其本身
     def eat(self, food_name: str):
         print(f'my name is {self.name}, I am {self.age} old, and I am eating {food_name}')
     
-    def main():
+def main():
         node = PersonNode('Floating', 22)
         node.eat('牛排')
