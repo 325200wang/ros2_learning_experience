1f0d5072fa5d4d5797ac72d50eab7b01a7b3e031
325200wang <a18967707065@icloud.com>
2025-11-18 16:26:10 +0800

    11.18

--- chapter2/build/demo_python_pkg/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/media/dubhe/store/learning/ros2_learning_experience/chapter2/install/demo_cpp_pkg/share/demo_cpp_pkg/package.sh"
--- chapter2/build/demo_python_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/media/dubhe/store/learning/ros2_learning_experience/chapter2/install/demo_python_pkg'
