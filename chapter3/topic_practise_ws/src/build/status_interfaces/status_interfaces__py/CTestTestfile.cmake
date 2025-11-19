2a9571be832385750a717b47a2de38a4350b82d5
325200wang <a18967707065@icloud.com>
2025-11-19 23:12:46 +0800

    11.19

---
+++ chapter3/topic_practise_ws/src/build/status_interfaces/status_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/dubhe/store/learning/ros2_learning_experience/chapter3/topic_practise_ws/src/build/status_interfaces/status_interfaces__py
+# Build directory: /media/dubhe/store/learning/ros2_learning_experience/chapter3/topic_practise_ws/src/build/status_interfaces/status_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ chapter3/topic_practise_ws/src/build/status_publisher/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/media/dubhe/store/learning/ros2_learning_experience/chapter3/topic_practise_ws/src/install/status_interfaces/share/status_interfaces/package.sh"
