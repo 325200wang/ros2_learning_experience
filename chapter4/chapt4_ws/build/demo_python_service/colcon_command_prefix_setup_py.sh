bbf214b8584e258a1f7bd86075c1daf25b583432
325200wang <a18967707065@icloud.com>
2025-11-22 21:23:20 +0800

    11.22

---
+++ chapter4/chapt4_ws/build/demo_python_service/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/media/dubhe/store/learning/ros2_learning_experience/chapter4/chapt4_ws/install/chapt4_interfaces/share/chapt4_interfaces/package.sh"
