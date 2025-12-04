db05b631c723f7000dd3e0a01c3486dfbf1da557
325200wang <a18967707065@icloud.com>
2025-12-04 15:24:50 +0800

    tutorial

---
+++ chapter6/chapt6_ws/build/urdf_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/dubhe/store/learning/ros2_learning_experience/chapter6/chapt6_ws/src/urdf_tutorial
+# Build directory: /media/dubhe/store/learning/ros2_learning_experience/chapter6/chapt6_ws/build/urdf_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
